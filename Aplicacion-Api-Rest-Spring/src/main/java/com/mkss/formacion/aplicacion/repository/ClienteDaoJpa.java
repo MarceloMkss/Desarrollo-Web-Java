ddc6f6999ce5d9cad55001a7c197ad318a641492
Marcelo Kleibson <marcelomkss@gmail.com>
2021-02-03 20:01:57 +0100

    creo un clienteDaoJpa para hacer pruebas con jpa en el mismo proyecto

---
+++ Aplicacion-Api-Rest-Spring/src/main/java/com/mkss/formacion/aplicacion/repository/ClienteDaoJpa.java
@@ -0,0 +1,40 @@
+package com.mkss.formacion.aplicacion.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mkss.formacion.aplicacion.entity.Cliente;
+
+@Repository // si quiero usar el otro clienteDaoJdbcTemplate, tengo que poner esa anotacion(@repository)
+public class ClienteDaoJpa implements Dao<Cliente> {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public Iterable<Cliente> obtenerTodos() {
+		return repo.findAll();
+	}
+
+	@Override
+	public Cliente obtenerPorId(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public Cliente insertar(Cliente cliente) {
+		return repo.save(cliente);
+	}
+
+	@Override
+	public Cliente modificar(Cliente cliente) {
+		repo.save(cliente);
+		return cliente;
+	}
+
+	@Override
+	public void borrar(Long id) {
+		repo.deleteById(id);
+	}
+
+}
